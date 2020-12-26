7eaef4065f80c5c486bf840e21ab0a8ba7ad10a5
Tayfun Sur <tayfunsur@gmail.com>
2020-12-26 10:11:41 +0300

    Delete empty string control

--- database/job-database.js
+++ database/job-database.js
@@ -3,7 +3,7 @@ const Job = require('../models/job')
 
 class JobDatabase extends BaseDatabase {
   findByKeyword(keyword) {
-    if (keyword && keyword !== '') {
+    if (keyword) {
       const results = this.load().filter(o => o.title.toLowerCase().includes(keyword.toLowerCase()))
       results.forEach(job => console.log(job.title))
       return results
