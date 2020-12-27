c8f8f72c5ddd7a15558023fe43aa9830c9bf7618
Tayfun Sur <tayfunsur@gmail.com>
2020-12-27 22:50:04 +0300

    Extract the imports

--- database/employer-database.js
+++ database/employer-database.js
@@ -1,7 +1,5 @@
 const BaseDatabase = require('./base-database')
 const Employer = require('../models/employer')
-const fs = require('fs')
-const flatted = require('flatted')
 
 class EmployerDatabase extends BaseDatabase {
 
