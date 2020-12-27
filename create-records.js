dbb754312823c7bf258069d52fb6cf46b6258181
Tayfun Sur <tayfunsur@gmail.com>
2020-12-27 22:46:32 +0300

    Apply async-await

--- create-records.js
+++ create-records.js
@@ -5,23 +5,18 @@ const Freelancer = require('./models/freelancer')
 const { userDatabase, freelancerDatabase, employerDatabase, jobDatabase, orderDatabase } = require('./database')
 
 const user1 = User.create({ name: 'Leyla' })
-const employer1 = Employer.create({id: user1.id, activeRole:user1.activeRole, name:user1.name, messages:user1.messages})
-const freelancer1 = Freelancer.create({id: user1.id, activeRole:user1.activeRole, name:user1.name, messages:user1.messages})
+const employer1 = Employer.create(user1)
+const freelancer1 = Freelancer.create(user1)
 
 const user2 = User.create({ name: 'Mecnun'} )
-const employer2 = Employer.create({id: user1.id, activeRole:user1.activeRole, name:user1.name, messages:user1.messages})
-const freelancer2 = Freelancer.create({id:user2.id, activeRole:user2.activeRole, name:user2.name, messages:user2.messages})
-
-// const user3 = User.create({ name: 'Erdal' })
-// const employer3 = Employer.create({ user: user3 })
-// const freelancer3 = Freelancer.create({ user: user3 })
+const employer2 = Employer.create(user2)
+const freelancer2 = Freelancer.create(user2)
 
 freelancer2.addSpecialty('Web Development', 10)
 freelancer2.updateProfile('Turkey', 'I created Facebook')
 
 const job1 = freelancer2.createJob({title: 'Photoshop', content: 'Photoshop Master', price:  30, deliveryTime: 3 })
 const job2 = freelancer2.createJob({title: 'Wordpress', content: 'All kind of websites', price: 50, deliveryTime:  7 })
-
 
 const order1 = employer1.buy(job1)
 const order2 = employer1.buy(job2)
@@ -32,8 +27,27 @@ user2.sendMessage(user1, job1, 'A sample response')
 employer1.rateAndComment(order1, 4, 'Great job!')
 employer1.rateAndComment(order2, 2, 'Shitty job!')
 
-userDatabase.save([user1, user2])
-employerDatabase.save([employer1, employer2])
-freelancerDatabase.save([freelancer1, freelancer2])
-jobDatabase.save([job1, job2])
-orderDatabase.save([order1, order2])
+
+async function main() {
+  try {
+    await userDatabase.save([user1, user2])
+    await employerDatabase.save([employer1, employer2])
+    await freelancerDatabase.save([freelancer1, freelancer2])
+    await jobDatabase.save([job1, job2])
+    await orderDatabase.save([order1, order2])
+
+    const user3 = User.create({ name: 'Erdal' })
+    const employer3 = Employer.create(user3)
+    const freelancer3 = Freelancer.create(user3)
+    
+    await userDatabase.insert(user3)
+    await employerDatabase.insert(user3)
+    await freelancerDatabase.insert(user3)
+
+  } catch (e) {
+    console.log(e)
+  }
+
+}
+
+main()
