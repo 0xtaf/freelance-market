347378916ae9f3c98c5b91b90fec386b8c78ae52
Tayfun Sur <tayfunsur@gmail.com>
2020-12-27 22:49:03 +0300

    Delete the load function

--- database/employer-database.js
+++ database/employer-database.js
@@ -4,11 +4,7 @@ const fs = require('fs')
 const flatted = require('flatted')
 
 class EmployerDatabase extends BaseDatabase {
-  load() {
-    const file = fs.readFileSync(`${__dirname}/${this.filename}.json`, 'utf8')
-    const objects = flatted.parse(file)
-    return objects.map(this.model.create)
-  }
+
 }
 
 module.exports = new EmployerDatabase(Employer)
