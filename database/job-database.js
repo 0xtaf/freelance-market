c1993bbbf284750e987d8d7f85ee3be73de4dd15
Tayfun Sur <tayfunsur@gmail.com>
2020-12-28 23:53:45 +0300

    Delete unnecessary await words

--- database/job-database.js
+++ database/job-database.js
@@ -4,10 +4,10 @@ const Job = require('../models/job')
 class JobDatabase extends BaseDatabase {
   async findByKeyword(keyword) {
     const objects = await this.load()
-    const results = await objects.filter((o) => o.title.toLowerCase().includes(keyword.toLowerCase()))
-    await results.forEach(job => console.log(job.title))
+    const results = objects.filter((o) => o.title.toLowerCase().includes(keyword.toLowerCase()))
+    results.forEach(job => console.log(job.title))
 
-    return await results
+    return results
   }
 }
 
