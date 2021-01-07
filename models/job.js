db44a1e6bcf29b70349fcb6d921806ed999ded57
Tayfun Sur <tayfunsur@gmail.com>
2021-01-07 08:21:17 +0300

    Add ratingsAndComments to the constructor

--- models/job.js
+++ models/job.js
@@ -1,18 +1,19 @@
 const { v4: uuidv4 } = require('uuid')
 
 class Job {
-  constructor(id = uuidv4(), freelancer, title, content, price, deliveryTime, employers=[]){
+  constructor(id = uuidv4(), freelancer, title, content, price, deliveryTime, ratingsAndComments = [] , employers=[]){
     this.id = id
     this.freelancer = freelancer
     this.title = title
     this.content = content
     this.price = price
     this.deliveryTime = deliveryTime
+    this.ratingsAndComments = ratingsAndComments
     this.employers = employers
   }
 
-  static create({id, freelancer, title, content, price, deliveryTime, employers}) {
-    return new Job(id, freelancer, title, content, price, deliveryTime, employers)
+  static create({id, freelancer, title, content, price, deliveryTime, ratingsAndComments, employers}) {
+    return new Job(id, freelancer, title, content, price, deliveryTime, ratingsAndComments, employers)
   }
 }
 
