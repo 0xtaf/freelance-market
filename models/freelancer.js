ab6784b28721e58523070f92eef9078f704916d6
Tayfun Sur <tayfunsur@gmail.com>
2021-01-10 09:00:55 +0300

    Push full job instance itself

--- models/freelancer.js
+++ models/freelancer.js
@@ -25,7 +25,7 @@ class Freelancer extends User {
 
   async createJob({title, content, price, deliveryTime}){
     const job = Job.create({freelancer: this, title, content, price, deliveryTime})
-    this.jobs.push(job.title)
+    this.jobs.push(job)
     return job
   }
 
