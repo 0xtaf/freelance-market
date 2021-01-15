11a43df656ba6ae09f3b6d92c11af935f3b422f6
Tayfun Sur <tayfunsur@gmail.com>
2021-01-15 23:20:26 +0300

    Change comment array's name

--- models/job.js
+++ models/job.js
@@ -1,19 +1,19 @@
 const { v4: uuidv4 } = require('uuid')
 
 class Job {
-  constructor(id = uuidv4(), freelancer, title, content, price, deliveryTime, ratingsAndComments = [] , employers=[]){
+  constructor(id = uuidv4(), freelancer, title, content, price, deliveryTime, comments = [] , employers=[]){
     this.id = id
     this.freelancer = freelancer
     this.title = title
     this.content = content
     this.price = price
     this.deliveryTime = deliveryTime
-    this.ratingsAndComments = ratingsAndComments
+    this.comments = comments
     this.employers = employers
   }
 
-  static create({id, freelancer, title, content, price, deliveryTime, ratingsAndComments, employers}) {
-    return new Job(id, freelancer, title, content, price, deliveryTime, ratingsAndComments, employers)
+  static create({id, freelancer, title, content, price, deliveryTime, comments, employers}) {
+    return new Job(id, freelancer, title, content, price, deliveryTime, comments, employers)
   }
 }
 
